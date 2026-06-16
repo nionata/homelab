658a83a4764f8a2db0d56f6435f13319073f3fbc
Nick Ionata <n.ionata129@gmail.com>
2026-06-16 16:45:44 +0000

    add lab

--- configurations/homeroc/configuration.nix
+++ configurations/homeroc/configuration.nix
@@ -41,6 +41,11 @@
     }
   ];
 
+  homelab = {
+    enable = true;
+    hostName = "homeroc";
+  };
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
